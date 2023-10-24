0f6ad03477f18b70f1785fd434d274a697a4ff0b
Sadam Manikantam <96649127+smanikantam@users.noreply.github.com>
2023-10-25 04:13:22 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -134,5 +134,5 @@ STATIC_ROOT=BASE_DIR / "staticfiles_build" / "static"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL='/media/'
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+# MEDIA_URL='/media/'
+# MEDIA_ROOT=os.path.join(BASE_DIR,'media')
