c4cae3778bcf31c7c3846ca8eb2909f4c6ac3d53
Sadam Manikantam <96649127+smanikantam@users.noreply.github.com>
2023-10-25 02:06:26 +0530

    Update models.py

--- home/models.py
+++ home/models.py
@@ -1,20 +1,18 @@
 from django.db import models
 from django import forms
-from .models import Fyles
-
-
-# Create your models here.
 
 class Fyles(models.Model):
     file = models.FileField()
-    name=models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     date = models.DateField(auto_now_add=True)
-    added_by=models.CharField(max_length=25)
-    file_size=models.CharField(max_length=25)
-    class FileUploadForm(forms.ModelForm):
-        class Meta:
-            model = Fyles
-            fields = ['name', 'added_by', 'file']
+    added_by = models.CharField(max_length=25)
+    file_size = models.CharField(max_length=25)
+
+# Define a separate form class for file uploads
+class FileUploadForm(forms.ModelForm):
+    class Meta:
+        model = Fyles
+        fields = ['name', 'added_by', 'file']
 
 
 
