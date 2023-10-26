6d454bfcd54edc6cd4be085b422d30dce98cf351
Sadam Manikantam <96649127+smanikantam@users.noreply.github.com>
2023-10-26 19:18:36 +0530

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ urlpatterns=[
     path('',views.index ,name='index'),
     path('file_list', views.file_list, name='file_list'),
     path('upload_file', views.upload_file, name='upload_file'),
+    path('delete_entry', views.delete_entry, name='delete_entry'),
 ]
